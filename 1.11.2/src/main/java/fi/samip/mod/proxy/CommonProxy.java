69e0d6a03e5498f997f9dda1e65778b81cdc37f4
samip5 <saku537@gmail.com>
2017-04-22 02:56:58 +0300

    Rendered the config button in GUI not-usable.

--- 1.11.2/src/main/java/fi/samip/mod/proxy/CommonProxy.java
+++ 1.11.2/src/main/java/fi/samip/mod/proxy/CommonProxy.java
@@ -8,6 +8,7 @@ import fi.samip.mod.util.ModUtil;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
 public class CommonProxy {
@@ -22,20 +23,19 @@ public class CommonProxy {
 		
 		ModUtil.LOGGER.info("PreInitialization Finished.");
 	}
+	
 	public void init(FMLInitializationEvent event) {
 		ModUtil.LOGGER.info("Starting Initialization Phase...");
-		GameRegistry.registerWorldGenerator(new GenHandler(), 1);
-		RecipeHandler.registerCraftingRecipes();
-		RecipeHandler.registerFurnaceRecipes();
+		
 		ModUtil.LOGGER.info("Initialization Finished.");
 	}
+	
 	public void postInit(FMLPostInitializationEvent event) {
 		ModUtil.LOGGER.info("Starting PostInitialization Phase...");
 		ModUtil.LOGGER.info("PostInitialization Finished.");
 	}
 	
-	public void loadConfiguration() {
-		//general.saltPerChunk = SamipFoods.configuration.get(Configuration.CATEGORY_GENERAL, "saltPerChunk", 4);
+	public void serverStarting(FMLServerStartingEvent event) {
 	}
 	
 }
